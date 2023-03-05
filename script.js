023e53480393aae86a4b6341dfa2dface3c21455
Oleg <8888kb8888@gmail.com>
2023-03-05 18:19:57 +0300

    Урок 2

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let inputIn = document.querySelector('.input-in');   // input
 let button = document.querySelector('button');   //button
+let div = document.querySelector('.out');
 
 button.onclick = function () {
     // кнопка будет нажата
     console.log('Работает');
     // console.log(inputIn.value);   // value - то что введено в input
-    let b = inputIn.value;   // сокращение предыдущего + преобразовует в число
+    let b = +inputIn.value;   // сокращение предыдущего + преобразовует в число
     console.log(b + 2);
+    div.innerHTML = b;      // перенос результата на HTML
+    inputIn.value = '';   // очищает
 }
