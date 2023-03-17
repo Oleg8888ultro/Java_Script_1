6cf6a476d04271ded70bb2e0ea92ef2f28bad855
Oleg <8888kb8888@gmail.com>
2023-03-17 17:33:37 +0300

    применение forEach

--- lesson number 3/script.js
+++ lesson number 3/script.js
@@ -1,3 +1,13 @@
-document.querySelector('button').onclick = function () {
+document.querySelector('button').onclick = myFunc
+function myFunc() {
+    console.log(document.querySelector('#one').value);
+    // style
+    // document.querySelector('p').style.color =
+    // стили будут относиться к первому абзаццу
+    // document.querySelector('#one').value
 
+    document.querySelectorAll('p').forEach((el) => {
+        el.style.color = document.querySelector('#one').value;
+    });
+    // пройтись циклом forEach 
 }
