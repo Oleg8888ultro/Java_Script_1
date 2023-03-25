13a848fac5f9121fcbab023d94e43fa1891fbac8
oleg <8888kb8888@gmail.com>
2023-03-25 12:10:19 +0300

    Работа с функциями

---
+++ test_funkci/module/function.js
@@ -0,0 +1,3 @@
+export const sayHello = (name, surname) => {
+    console.log('Hello, ' + name + ' ' + surname + '!');
+};
---
+++ test_funkci/module/solution.js
@@ -0,0 +1,2 @@
+import { sayHello } from './functions.js'
+sayHello('Jack', 'Jackson'); // Hello, Jack Jackson!
